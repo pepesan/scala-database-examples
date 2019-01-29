e711ec3b5a7f51f336d4909fa02f6375c83d2637
pepesan <pepesan@gmailcom>
2019-01-29 16:23:00 +0100

    corregido driver para mysql8

--- src/main/scala/com/cursosdedesarrollo/Ejemplo01Conexion.scala
+++ src/main/scala/com/cursosdedesarrollo/Ejemplo01Conexion.scala
@@ -8,7 +8,7 @@ object Ejemplo01Conexion extends App {
 
     // connect to the database named "mysql" on port 8889 of localhost
     val url = "jdbc:mysql://localhost:3306/mysql"
-    val driver = "com.mysql.jdbc.Driver"
+    val driver = "com.mysql.cj.jdbc.Driver"
     val username = "root"
     val password = "root"
     var connection: Connection = null
